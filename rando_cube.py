faa69346d02d6f9df7c108817b2f4e567bc531c1
Yegor Kozhevnikov <ykozhevn@gmail.com>
2025-05-12 19:49:54 -0400

    Added Bevel mod and Auto Smooth

--- rando_cube.py
+++ rando_cube.py
@@ -25,3 +25,12 @@ for _ in range(CUBE_NUM):
             
     if flag:    
         bpy.ops.mesh.primitive_cube_add(size=CUBE_SIZE, enter_editmode=False, align='WORLD', location=(x,y,z), scale=(1, 1, 1))
+
+bpy.ops.object.select_all(action='SELECT')
+
+for obj in C.selected_objects:
+    mod = obj.modifiers.new("Bevel", 'BEVEL')
+    mod.width = .4
+    mod.segments = 4
+    
+bpy.ops.object.shade_auto_smooth()
