87efeededdf43dda19f3e98f63d70e21f18670b3
Yegor Kozhevnikov <ykozhevn@gmail.com>
2025-05-12 15:37:46 -0400

    Fix spawn

--- rando_cube.py
+++ rando_cube.py
@@ -3,8 +3,7 @@ import random
 
 for _ in range(5):
 
-    x, y, z = 3 * [random.randint(-5,5)]
+    x, y, z = random.randint(-5,5), random.randint(-5,5), random.randint(-5,5)
     
-    for 
     bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(x,y,z), scale=(1, 1, 1))
 
