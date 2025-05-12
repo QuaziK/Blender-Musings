31237f614f231c1f73d2e6b307d515f30023b3b6
Yegor Kozhevnikov <ykozhevn@gmail.com>
2025-05-12 16:47:46 -0400

    Fixed Touching Issue

--- rando_cube.py
+++ rando_cube.py
@@ -1,9 +1,25 @@
 import bpy
 import random
+import mathutils
+import math
 
-for _ in range(5):
+def vector_len(v1):
+    return math.sqrt(v1.x**2 + v1.y**2 + v1.z**2)
 
-    x, y, z = random.randint(-5,5), random.randint(-5,5), random.randint(-5,5)
-    
-    bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(x,y,z), scale=(1, 1, 1))
+C = bpy.context
+D = bpy.data
+CUBE_SIZE = 2
+VECTOR_THRESHHOLD = vector_len(mathutils.Vector((CUBE_SIZE,CUBE_SIZE,CUBE_SIZE)))
 
+for _ in range(15):
+
+    x, y, z = random.randint(-5,5),random.randint(-5,5),random.randint(-5,5)
+
+    flag = True
+
+    for obj in D.objects:
+        if vector_len(mathutils.Vector((x,y,z)) - obj.location) <= VECTOR_THRESHHOLD:
+            flag = False
+            
+    if flag:    
+        bpy.ops.mesh.primitive_cube_add(size=CUBE_SIZE, enter_editmode=False, align='WORLD', location=(x,y,z), scale=(1, 1, 1))
